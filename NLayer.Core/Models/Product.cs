242ad5f0ab8321bbededcbb971dc32844925ca3a
tuggcebolat <tuggce20@gmail.com>
2023-09-25 16:22:41 +0300

    Add project files.

---
+++ NLayer.Core/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace NLayer.Core
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; } //foreignkey
+
+        public Category Category { get; set; } //bir ürünün bir kategorisi olabilir
+
+        public ProductFeature ProductFeature { get; set; }  //bir ürünün özellikleri var
+    }
+}
---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+
+        IQueryable Where(Expression<Func<T, bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+
+    }
+}
